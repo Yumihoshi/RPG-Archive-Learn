76b1a6d3f27b843ff2c73d1c2d0acfe8cbe982dd
Yumihoshi <xiaoyuesun915@gmail.com>
2025-06-23 16:54:05 +0800

    【功能】 火宝可梦派生类
    Signed-off-by: Yumihoshi <xiaoyuesun915@gmail.com>

--- RPG-Archive-Learn/FirePokeModel.h
+++ RPG-Archive-Learn/FirePokeModel.h
@@ -1,5 +1,11 @@
 #pragma once
-class FirePokeModel
+#include "PokemonModel.h"
+class FirePokeModel : public PokemonModel
 {
+public:
+
+private:
+    bool _isKuangBao = false;       // 是否处于狂暴状态
+    int _KuangBaoZengShang;         // 狂暴增伤数值
 };
 
