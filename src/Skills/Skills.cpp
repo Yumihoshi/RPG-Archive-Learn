9e78136f6508c16a9a20aeb3f8be19f19535109d
慕辽 <3042265206@qq.com>
2025-06-30 17:59:41 +0800

    技能

--- src/Skills/Skills.cpp
+++ src/Skills/Skills.cpp
@@ -1,32 +1,41 @@
 //
 // Created by ASUS on 25-6-30.
 //
-#include "Skills.h"
-#include "../Managers/LogManager.h"
-#include "../Common/Types.h"
-#include "../Managers/FightManager.h"
+#include "../../include/Skills/Skills.h"
+#include "../../include/Managers/LogManager.h"
+#include "../../include/Common/Types.h"
+#include "../../include/Managers/FightManager.h"
 
-void SkillHandler::useSkill(Skills skill) {
+void SkillHandler::useSkill(Skills skill)
+{
     const auto trigger = FightManager::GetInstance().GetPlayerFightPoke();
     const auto target = FightManager::GetInstance().GetEnemyFightPoke();
+
     switch (skill) {
-        case Skills::Fire:
+        case Skills::Fire:{
+            auto m = std:: dynamic_pointer_cast<FirePokeModel>(trigger->ModelPtr);
             LogManager::PrintByChar(trigger->ModelPtr->GetName() + " 使用了烈火连爆！\n");
-            
-            break;
-        case Skills::Ice:
+            m -> CritCount+=5;
+            break;}
+        case Skills::Ice: {
             LogManager::PrintByChar(trigger->ModelPtr->GetName() + " 使用了寒霜凝滞！\n");
             break;
-        case Skills::Grass:
+        }
+        case Skills::Grass: {
             LogManager::PrintByChar(trigger->ModelPtr->GetName() + " 使用了青藤焕生！\n");
             trigger->ModelPtr->Heal(20);
             break;
-        case Skills::Fly:
+        }
+        case Skills::Fly: {
             LogManager::PrintByChar(trigger->ModelPtr->GetName() + " 使用了空影迷踪！\n");
+            target->ModelPtr->SetFleeRate(target->ModelPtr->GetFleeRate() + 0.1);
             break;
-        case Skills::Ghost:
+        }
+        case Skills::Ghost: {
             LogManager::PrintByChar(trigger->ModelPtr->GetName() + " 使用了鬼雾蚀力！\n");
+            target->ModelPtr->SetDamage(target->ModelPtr->GetDamage()*0.9);
             break;
+        }
         default:
             break;
     }
